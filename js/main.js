65bb6d63eac4627d7a27db797ddb3470420f132a
LeulAria <leulsegedtariku@gmail.com>
2020-05-03 00:39:40 -1200

    test commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function darkTheme() {
+  document.documentElement.style.setProperty('--primary', '#333');
+  document.documentElement.style.setProperty('--secondary', '#f4f4f4');
+  document.documentElement.style.setProperty('--dark-primary', '#2a2a2a');
+  document.documentElement.style.setProperty('--light-primary', '#d0d0d0');
+}; 
+
+function lightTheme() {
+  document.documentElement.style.setProperty('--primary', '#f4f4f4');
+  document.documentElement.style.setProperty('--secondary', '#333');
+  document.documentElement.style.setProperty('--dark-primary', '#e7e7e7');
+  document.documentElement.style.setProperty('--light-primary', '#555');
+}; 
---
+++ pages/xo-game/precache-manifest.5e1654baba62d36e02d7ac5362faf7dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "33f107081e087b1175b811903d9ffab6",
+    "url": "/static/media/play.33f10708.png"
+  },
+  {
+    "revision": "04d2c8c31d30e2d7f2d43573ca19cf09",
+    "url": "/static/media/pause.04d2c8c3.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4103b98b6299e784c64b",
+    "url": "/static/js/main.4103b98b.chunk.js"
+  },
+  {
+    "revision": "61fbc54d5278bc8a28aa",
+    "url": "/static/js/2.61fbc54d.chunk.js"
+  },
+  {
+    "revision": "4103b98b6299e784c64b",
+    "url": "/static/css/main.649b2559.chunk.css"
+  },
+  {
+    "revision": "9cb3df1d2f2832969ffad47e9e49361f",
+    "url": "/index.html"
+  }
+];
